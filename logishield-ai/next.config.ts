95d47539b1cc30079a8aa6e22746429759d25303
LabrecheFatima <fatimalabreche438@gmail.com>
2026-06-04 18:49:58 +0100

    configure docker and standalone output

--- logishield-ai/next.config.ts
+++ logishield-ai/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'standalone'
 };
 
 export default nextConfig;
